7826203d6eadfadddf318f0c7120dd4679b3b6d5
SAG145 <150883378+SAG145@users.noreply.github.com>
2024-04-10 22:19:39 +0300

    Add files via upload

---
+++ q235 - An Arithmetic Geometric Sequence.py
@@ -0,0 +1,20 @@
+def u(k,r):
+    return (900 - 3*k)*r**(k-1)
+
+def s(r):
+    sum = 0
+    for k in range(1,5001):
+        sum += u(k,r)
+    return sum
+
+guess = 0
+power = 0
+while power > -15:
+    for k in range(10):
+        if s(guess + k*10**power) < -600000000000:
+            guess += (k-1)*10**power
+            power -= 1
+            break
+print(round(10**12*guess) / 10**12)
+
+#answer = 1.002322108633
