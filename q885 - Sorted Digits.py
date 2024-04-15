f0fdcf407989771ea94616c08de38415cf33b37c
SAG145 <150883378+SAG145@users.noreply.github.com>
2024-04-16 01:51:10 +0300

    Add files via upload

---
+++ q885 - Sorted Digits.py
@@ -0,0 +1,32 @@
+from math import factorial
+def factorial_list(lst):
+    m = 1
+    for i in lst:
+        m *= factorial(i)
+    return m
+
+def digits_to_num(digits):
+    n = "0"
+    for d in range(10):
+        n += str(d)*digits[d]
+    return int(n)
+
+def S(n):
+    x = 0
+    for a1 in range(n + 1):
+        for a2 in range(n - a1 + 1):
+            for a3 in range(n - a1 - a2 + 1):
+                for a4 in range(n - a1 - a2 - a3 + 1):
+                    for a5 in range(n - a1 - a2 - a3 - a4 + 1):
+                        for a6 in range(n - a1 - a2 - a3 - a4 - a5 + 1):
+                            for a7 in range(n - a1 - a2 - a3 - a4 - a5 - a6 + 1):
+                                for a8 in range(n - a1 - a2 - a3 - a4 - a5 - a6 - a7 + 1):
+                                    for a9 in range(n - a1 - a2 - a3 - a4 - a5 - a6 - a7 - a8 + 1):
+                                        a0 = n - (a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9)
+                                        digits = [a0,a1,a2,a3,a4,a5,a6,a7,a8,a9]
+                                        x = (x + digits_to_num(digits)*factorial(n) // factorial_list(digits)) % 1123455689
+    return x
+
+print(S(18))
+
+#answer = 827850196
