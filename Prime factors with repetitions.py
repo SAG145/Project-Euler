962fa6ad4efa83ceeee36048df265ca1a4bf79f8
SAG145 <150883378+SAG145@users.noreply.github.com>
2024-04-27 19:48:07 +0300

    Update Prime factors with repetitions.py

--- Prime factors with repetitions.py
+++ Prime factors with repetitions.py
@@ -1,13 +1,11 @@
-def prime_factors_with_repetitions(n,primes_list): #בעת הקריאה לפונקציה יש להוסיף [] במקום של ה primes_list
-    if n % 2 == 0:                                 #אחרת בהפעלה חוזרת הפונקציה לא תפעל
-        primes_list.append(2)
-        return prime_factors_with_repetitions(n/2,primes_list)
-    elif is_prime(n):
-        primes_list.append(int(n))
-        return primes_list
+import math
+def prime_factors_with_repetitions(n,primes_list = [],m = 2):
+    if n % 2 == 0:
+        return prime_factors_with_repetitions(n // 2,primes_list + [2])
     else:
-        for k in range(3,int(math.sqrt(n))+1,2):
+        if m == 2:
+            m = 3
+        for k in range(m,int(math.sqrt(n)) + 1,2):
             if n % k == 0:
-                if is_prime(k):
-                    primes_list.append(k)
-                    return prime_factors_with_repetitions(n/k,primes_list)
+                return prime_factors_with_repetitions(n // k,primes_list + [k],m)
+        return primes_list + [n]
