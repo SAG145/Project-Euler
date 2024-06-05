f5bd0ef6d47e4f894b861099ab1f745c6122802f
SAG145 <150883378+SAG145@users.noreply.github.com>
2024-06-05 07:28:33 +0300

    Delete is Permutation.py

--- is Permutation.py
+++
@@ -1,7 +0,0 @@
-def is_permutation(n1,n2):
-    lst1,lst2 = [],[]
-    for k in str(n1):
-        lst1.append(int(k))
-    for j in str(n2):
-        lst2.append(int(j))
-    return sorted(lst1) == sorted(lst2)
