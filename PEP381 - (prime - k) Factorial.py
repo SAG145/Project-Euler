51fba6e4e161d92dd94efb25ddfc741a95c8b9c2
SAG145 <150883378+SAG145@users.noreply.github.com>
2024-08-12 17:16:27 +0300

    Add files via upload

---
+++ PEP381 - (prime - k) Factorial.py
@@ -0,0 +1,34 @@
+import math
+def all_primes_below_n(n):
+    primes_bool = [False, False] + [True] * (n - 2)
+    for k in range(2, int(math.sqrt(n)) + 1):
+        if primes_bool[k]:
+            for l in range(2*k,n,k):
+                primes_bool[l] = False
+    primes_list = []
+    for k in range(n):
+        if primes_bool[k]:
+            primes_list.append(k)
+    return primes_list
+
+def extended_Euclid(n,mod):
+    if n < mod:
+        return extended_Euclid(mod,n)[::-1]
+    while n % mod != 0:
+        i = extended_Euclid(mod,n % mod)
+        q = n // mod
+        return (i[1],i[0] - q*i[1])
+    return (0,1)
+
+def inverse(n,mod):
+    return extended_Euclid(n,mod)[0] % mod
+
+def S(p):
+    return (inverse(p - 2,p) + inverse((p - 2)*(p - 3),p) + inverse((p - 2)*(p - 3)*(p - 4),p)) % p
+
+sigma = 0
+for p in all_primes_below_n(10**8)[2:]:
+    sigma += S(p)
+print(sigma)
+
+#answer = 139602943319822
