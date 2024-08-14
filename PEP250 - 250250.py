902525a58b269782c71992b60f94d480bbcae037
SAG145 <150883378+SAG145@users.noreply.github.com>
2024-08-14 05:11:54 +0300

    Add files via upload

---
+++ PEP250 - 250250.py
@@ -0,0 +1,20 @@
+import copy
+def next_num(n,mods_sums,mod):
+    new = copy.copy(mods_sums)
+    for m in range(250):
+        new[(m + n) % 250] = (new[(m + n) % 250] + mods_sums[m]) % mod
+    return new
+
+mod = 10**16
+mods = []
+for a in range(1,250251):
+    mods.append(pow(a,a,250))
+mods_sums = [1] + [0]*249
+i = 1
+for k in mods:
+    mods_sums = next_num(k,mods_sums,mod)
+    i += 1
+
+print(mods_sums[0] - 1)
+
+#answer = 1425480602091519
