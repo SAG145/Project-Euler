dd976a5110ef54e0f46251f38b3fbb3dc1ac1b74
SAG145 <150883378+SAG145@users.noreply.github.com>
2024-08-23 22:25:59 +0300

    Add files via upload

---
+++ PEP743 - Window into a Matrix.py
@@ -0,0 +1,35 @@
+from time import perf_counter
+start = perf_counter()
+def extended_Euclid(n,mod):
+    if n < mod:
+        return extended_Euclid(mod,n)[::-1]
+    while n % mod != 0:
+        i = extended_Euclid(mod,n % mod)
+        q = n // mod
+        return (i[1],i[0] - q*i[1])
+    return (0,1)
+
+def inverse(n,mod):
+    return extended_Euclid(n,mod)[0] % mod
+
+k = 10**8
+n = 10**16
+mod = 10**9 + 7
+facs = [1]
+inverses = [1]
+for i in range(1,k + 1):
+    facs.append(i*facs[-1] % mod)
+    if i % 2 == 0 or i <= k // 2:
+        inverses.append(inverse(facs[-1],mod))
+    else:
+        inverses.append("")
+
+power = pow(2,n // k,mod)
+a = 0
+for ones in range(0,k + 1,2):
+    a += facs[-1]*inverses[ones]*inverses[(k - ones) // 2]**2*pow(power,ones,mod)
+
+print(a % mod)
+print(perf_counter() - start)
+#answer = 259158998
+#
