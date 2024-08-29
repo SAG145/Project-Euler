3e9add800aacd2be2d12039153428c72d87a43b5
SAG145 <150883378+SAG145@users.noreply.github.com>
2024-08-29 16:19:07 +0300

    Add files via upload

---
+++ PEP686 - Powers of Two.py
@@ -0,0 +1,32 @@
+def next_a(a: object, p2: object) -> object:
+    return str(a*p2)[:4000]
+
+a = 2**90
+j = 90
+c123 = 1
+m = [2**196,2**289,2**485]
+while c123 < 678910:
+    na = next_a(a,m[0])
+    found = False
+    if na[:3] == "123":
+        found = True
+        a = int(na)
+        c123 += 1
+        j += 196
+    na = next_a(a,m[1])
+    if not found and na[:3] == "123":
+        found = True
+        a = int(na)
+        c123 += 1
+        j += 289
+    na = next_a(a,m[2])
+    if not found and na[:3] == "123":
+        found = True
+        a = int(na)
+        c123 += 1
+        j += 485
+
+print(j)
+
+#answer = 193060223
+#זמן הרצה - 13 דקות
