149e2e4c0bc86cf83c560b5ef53b7fdc210736d9
SAG145 <150883378+SAG145@users.noreply.github.com>
2024-09-02 07:52:37 +0300

    Add files via upload

---
+++ PEP169 - Sums of Powers of Two.py
@@ -0,0 +1,16 @@
+def f(n):
+    b = bin(n)[2:][::-1]
+    num_of_ways_leading_one = 1
+    num_of_ways_leading_zero = b.index(("1"))
+    b = b[b.index("1") + 1:]
+    while len(b) > 0:
+        i = b.index("1")
+        a = num_of_ways_leading_one + num_of_ways_leading_zero
+        num_of_ways_leading_zero = i*num_of_ways_leading_one + (i + 1)*num_of_ways_leading_zero
+        num_of_ways_leading_one = a
+        b = b[i + 1:]
+    return num_of_ways_leading_zero + num_of_ways_leading_one
+
+print(f(10**25))
+
+#answer = 178653872807
