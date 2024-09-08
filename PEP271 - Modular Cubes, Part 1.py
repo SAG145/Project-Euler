07ef4745eaceb167af908a5a460d92038869f11f
SAG145 <150883378+SAG145@users.noreply.github.com>
2024-09-08 19:51:06 +0300

    Update PEP271 - Modular Cubes, Part 1.py

--- PEP271 - Modular Cubes, Part 1.py
+++ PEP271 - Modular Cubes, Part 1.py
@@ -16,23 +16,23 @@ def solution(a,n,mod):
 def chinese_reminder(a,n,b,m):
     return (solution(a,n,n*m) + solution(b,m,n*m)) % (n*m)
 
-def base_C(p):
+def base_S(p):
     s = []
     for k in range(p):
         if k**3 % p == 1:
             s.append(k)
     return s
 
-def C(solutions,primes,mod):
+def S(solutions,primes,mod):
     if len(primes) == 0:
         return sum(solutions) - 1
     if len(solutions) == 0:
-        return C(base_C(primes[0]),primes[1:],primes[0])
+        return S(base_S(primes[0]),primes[1:],primes[0])
     new = []
     for k in base_C(primes[0]):
         for sol in solutions:
             new.append(chinese_reminder(sol,mod,k,primes[0]))
-    return C(new,primes[1:],mod*primes[0])
+    return S(new,primes[1:],mod*primes[0])
 
 print(C([],[2,3,5,7,11,13,17,19,23,29,31,37,41,43],1))
 
