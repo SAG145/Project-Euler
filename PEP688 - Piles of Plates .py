1ef8fc6596baa3a60ff46162de54e1fb23b6129c
SAG145 <150883378+SAG145@users.noreply.github.com>
2024-10-08 05:21:01 +0300

    Add files via upload

---
+++ PEP688 - Piles of Plates .py
@@ -0,0 +1,21 @@
+def tri(n):
+    return n*(n + 1) // 2
+
+def S(n):
+    s = 0
+    k = 1
+    while True:
+        t = tri(k)
+        if t > n:
+            break
+        m = (n - t + k) // k - 1
+        s += tri(m)*k
+        s += (m + 1)*((n - t) % k + 1)
+        s %= 10**9 + 7
+        k += 1
+    return s
+
+print(S(10**16))
+
+#answer = 110941813
+#3min
