e17c07ded98314b4852129acc18f2f13b3eca399
SAG145 <150883378+SAG145@users.noreply.github.com>
2024-11-25 04:20:19 +0200

    Add files via upload

---
+++ PEP918 - Recursive Sequence Summation.py
@@ -0,0 +1,26 @@
+import math
+
+def an(n):
+    if n == 1:
+        return 1
+    if n % 2 == 0:
+        return 2*an(n // 2)
+    return an(n // 2) - 3*an(n // 2 + 1)
+
+def S(n):
+    s = 0
+    m = 1
+    while m < n:
+        if 3*m < n:
+            s -= 5*m
+        s += m
+        q = n // m
+        if q % 4 == 1 or q % 4 == 2:
+            if 2**round(math.log(q - 1 + q % 2,2)) != q - 1 + q % 2:
+                s += m * an(q - 1 + q % 2)
+        m *= 2
+    return s
+
+print(S(10**12))
+
+#answer = -6999033352333308
