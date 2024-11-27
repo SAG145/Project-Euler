d96437d47b19c79fc51467d24843d7e8886347c3
SAG145 <150883378+SAG145@users.noreply.github.com>
2024-11-27 15:05:54 +0200

    Create PEP175 - Fractions and Sum of Powers of Two.py

---
+++ PEP175 - Fractions and Sum of Powers of Two.py
@@ -0,0 +1,32 @@
+def gcd(x,y):
+
+    while x % y != 0:
+
+        a = x
+
+        b = y
+
+        y = a % b
+
+        x = b
+
+    return y
+    
+def sbe_min(a,b):
+    g = gcd(a,b)
+    a //= g
+    b //= g
+    if a == 1:
+        return str(b)
+    if b == 1:
+        return "1," + str(a - 1)
+    if a > b:
+        q = a // b
+        return sbe_min(a % b,b) + "," + str(q)
+    q = b // a
+    return sbe_min(a,b % a) + "," + str(q)
+        
+   
+print(sbe_min(123456789,987654321))
+
+#answer = 1,13717420,8
