cf5c00adc10135bcfedd77609a9fee589fed2b7d
SAG145 <150883378+SAG145@users.noreply.github.com>
2025-01-10 06:46:38 +0200

    Add files via upload

---
+++ PEP182 - RSA Encryption.py
@@ -0,0 +1,20 @@
+def gcd(x,y):
+    while x % y != 0:
+        a = x
+        b = y
+        y = a % b
+        x = b
+    return y
+
+phi = 1008*3642
+
+mini_sum = 0
+
+for e in range(0,phi):
+    if gcd(e,phi) == 1:
+        if gcd((e - 1) // 2,phi) == 1:
+            mini_sum += e
+
+print(mini_sum)
+
+#answer = 399788195976
