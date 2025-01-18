4dbe68ca6ce912a2007a0b03107928f49eb71eea
SAG145 <150883378+SAG145@users.noreply.github.com>
2025-01-18 11:45:47 +0200

    Update PEP545 - Faulhaber's Formulas.py

--- PEP545 - Faulhaber's Formulas.py
+++ PEP545 - Faulhaber's Formulas.py
@@ -1,32 +1,59 @@
-def primes1(n):
-    bp = [True]*(n // 2)
-    for i in range(1,n + 1):
-        j = 1
-        while i + j + 2*i*j < len(bp) and j <= i:
-            bp[i + j + 2*i*j] = False
-            j += 1
-    primes = [2]
-    for k in range(1,len(bp)):
-        if bp[k]:
-            primes.append(2*k + 1)
-    return primes
+import math
+import random
 
-limit = 10**9
-sieve = [True]*limit
-pf = [2,3,5,23,29]
-for p in primes1(limit):
-    if sieve[p - 1] and p not in pf:
-        for i in range(p - 1,len(sieve),p - 1):
-            sieve[i] = False
+def all_primes_below_n(n):
+    primes_bool = [True]*n
+    for k in range(2, math.isqrt(n) + 1):
+        if primes_bool[k]:
+            # print(k)
+            for l in range(k**2,n,k):
+                primes_bool[l] = False
+    primes_list = []
+    for k in range(2,n):
+        if primes_bool[k]:
+            primes_list.append(k)
+    return primes_list
 
-f = 0
-for a in range(308,len(sieve),308):
-    if sieve[a]:
-        f += 1
-        if f == 10**5:
-            break
+def prob_prime(n):
+    for _ in range(10):
+        if pow(random.randrange(2,n),n - 1,n) != 1:
+            return False
+    return True
 
-print(a)
+def valid(n,divi):
+    for d in divi:
+        if prob_prime(n*d + 1):
+            if prob_prime(n*d + 1):
+                if prob_prime(n*d + 1):
+                    return False
+    return True
+
+divi = [2, 4, 14, 22, 28, 44, 154, 308]
+good_primes = []
+for p in all_primes_below_n(10**9 // 308):
+    if valid(p,divi):
+        good_primes.append(p)
+    if len(good_primes) == 10**5:
+        break
+
+good_numbers = []
+i = 0
+while i < len(good_primes):
+    j = 0
+    while j <= i and good_primes[i]*good_primes[j] < good_primes[-1]:
+        if valid(good_primes[i]*good_primes[j],divi):
+            good_numbers.append(good_primes[i]*good_primes[j])
+        k = 0
+        while k <= j and good_primes[i]*good_primes[j]*good_primes[k] < good_primes[-1]:
+            if valid(good_primes[i]*good_primes[j]*good_primes[k],divi) and valid(good_primes[i]*good_primes[j],divi) and valid(good_primes[i]*good_primes[k],divi) and valid(good_primes[j]*good_primes[k],divi):
+                good_numbers.append(good_primes[i]*good_primes[j]*good_primes[k])
+            k += 1
+        j += 1
+    i += 1
+    if j == 0:
+        break
+
+
+print(308*sorted(good_primes + good_numbers)[99998])
 
 #answer = 921107572
-#7 hours and 30 minutes
