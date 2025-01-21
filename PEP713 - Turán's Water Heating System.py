25701e268b55f4e1b0469f00dc669fe0456a904d
SAG145 <150883378+SAG145@users.noreply.github.com>
2025-01-22 01:57:16 +0200

    Create PEP713 - Turán's Water Heating System.py

---
+++ PEP713 - Turán's Water Heating System.py
@@ -0,0 +1,19 @@
+def c2(n):
+    return n*(n - 1) // 2
+    
+def T(N,m):
+    if m == 2:
+        return c2(N)
+    a = N // (m - 1)
+    b = N % (m - 1)
+    return b*c2(a + 1) + (m - b - 1)*c2(a)
+
+def L(N):
+    s = 0
+    for m in range(2,N + 1):
+        s += T(N,m)
+    return s
+
+print(L(10**7))
+
+#answer = 788626351539895
