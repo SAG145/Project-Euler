b4339480baf76e25a94d62ba00b1bed95d7d58f6
SAG145 <150883378+SAG145@users.noreply.github.com>
2025-03-02 04:51:18 +0200

    Add files via upload

---
+++ PEP872 - Recursive Tree.py
@@ -0,0 +1,23 @@
+import math
+def father1(n):
+    for i in range(100):
+        if 2**i + 1 > n:
+            return 2**(i - 1) + 1
+
+def plus_list(lst,plus):
+    new = []
+    for k in lst:
+        new.append(k + plus)
+    return new
+
+def path(n,k):
+    if k == 1:
+        f = father1(n)
+        if f == n:
+            return [f,1]
+        return path(n,f) + [1]
+    return plus_list(path(n - k + 1,1),k - 1)
+
+print(sum(path(10**17,9**17)))
+
+#answer = 2903144925319290239
