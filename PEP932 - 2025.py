75e060281215dd6d07ff3877583edf1a5045c86f
SAG145 <150883378+SAG145@users.noreply.github.com>
2025-03-09 16:46:13 +0200

    Add files via upload

---
+++ PEP932 - 2025.py
@@ -0,0 +1,14 @@
+def is_2025(n):
+    s = str(n)
+    l = len(s)
+    return s[l // 2] != "0" and ((int(s[:l // 2]) + int(s[l // 2:]))**2 == n or (l > 3 and (int(s[:l // 2 + 1]) + int(s[l // 2 + 1:]))**2 == n))
+
+s = 0
+for i in range(4,10**8):
+    if is_2025(i**2):
+        s += i**2
+
+print(s)
+
+#answer = 72673459417881349
+#2 minutes and 30 seconds
